86a3e9832b89c4f19c4a33ead40683375fc1b2d7
Azen Moktan <moktanazen@gmail.com>
2023-01-23 15:55:49 +0545

    Model Migration Created

---
+++ database/migrations/2023_01_23_093619_create_authors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string("name",120);
+            $table->string("email",120);
+            $table->string("password",120);
+            $table->string("phone_no");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+};
